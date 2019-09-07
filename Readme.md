dc69c28d866378db5734971b687f3ccddf8f9b1f
Zain Qureshi <zainnqureshii@gmail.com>
2019-09-07 14:03:22 +0500

    added whole project

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# JavaScript Left Rotation Array
--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const n1 = [];
 let n2 = [];
+var r = []
 
-function rotate(arr, rotation) {
-
+function rotate(arr, rotations) {
   for(let l = 1; l<=arr; l++) {
-    n1.push[l];
-    console.log(n1)
+    n1.push(l);
   }
 
+  console.log('Original Array', n1)
 
-  // for (let s = 1; s <= d; s++) {
-  //   var r = [...n2]
+  n2 = [...n1];
 
-  //   for (let i = n2.length - 1; i >= 0; i--) {
-  //     let tempIn = r[i - 1];
+  for (let s = 1; s <= rotations; s++) {
+    r = [...n2]
 
-  //     if (tempIn == r[-1]) {
-  //       tempR = r.lastIndexOf(n2[r.length - 1]);
-  //       r[tempR] = n2[i]
+    for (let i = n2.length - 1; i >= 0; i--) {
+      let tempIn = r[i - 1];
 
-  //     } else {
-  //       r[i - 1] = n2[i]
-  //     }
-  //   }
-  //   n2 = r;
-  // }
+      if (tempIn == r[-1]) {
+        tempR = r.lastIndexOf(n2[r.length - 1]);
+        r[tempR] = n2[i]
+
+      } else {
+        r[i - 1] = n2[i]
+      }
+    }
+    n2 = r;
+  }
 
 }
 
 
-rotate(5, 3);
+rotate(6,2);
 
-// console.log('Original ', n1)
-// console.log('New Array ', r)
+console.log('After Rotation ', r)
