da0876050592e6e543d30fb5ae5d70f98c0573d9
Zain Qureshi <zainnqureshii@gmail.com>
2019-09-07 13:56:35 +0500

    added whole project

---
+++ index.js
@@ -0,0 +1,35 @@
+const n1 = [];
+let n2 = [];
+
+function rotate(arr, rotation) {
+
+  for(let l = 1; l<=arr; l++) {
+    n1.push[l];
+    console.log(n1)
+  }
+
+
+  // for (let s = 1; s <= d; s++) {
+  //   var r = [...n2]
+
+  //   for (let i = n2.length - 1; i >= 0; i--) {
+  //     let tempIn = r[i - 1];
+
+  //     if (tempIn == r[-1]) {
+  //       tempR = r.lastIndexOf(n2[r.length - 1]);
+  //       r[tempR] = n2[i]
+
+  //     } else {
+  //       r[i - 1] = n2[i]
+  //     }
+  //   }
+  //   n2 = r;
+  // }
+
+}
+
+
+rotate(5, 3);
+
+// console.log('Original ', n1)
+// console.log('New Array ', r)
